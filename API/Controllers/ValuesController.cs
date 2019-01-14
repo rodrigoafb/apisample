5da5ad5d7c276248fcaadfad0928b9c9f9056d0d
Rodrigo Brito <rbrito@micromidia-1.local>
2019-01-14 17:27:32 -0200

    adding files

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		[Route("filter")]
+		[HttpGet]
+		public ActionResult<string> Get(bool value)
+		{
+			return $"Your value is {value}";
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return $"value {id}";
+		}
+
+		// POST api/values
+		[HttpPost]
+		public ActionResult<string> Post([FromBody] string value)
+		{
+			return $"Posted value {value}";
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public ActionResult<string> Put(int id, [FromBody] string value)
+		{
+			return $"Put value => id:{id}, content: {value}";
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public ActionResult<string> Delete(int id)
+		{
+			return $"Deleted value {id}";
+		}
+	}
+}
---
+++ APIClient/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace APIClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			using (var httpClient = new HttpClient())
+			{
+				var response = httpClient.GetAsync("http://localhost:55055/API/values").Result;
+
+				Console.WriteLine("GET 1 -------");
+				Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+				Console.WriteLine(string.Empty);
+
+				response = httpClient.GetAsync("http://localhost:55055/API/values/12").Result;
+
+				Console.WriteLine("GET 2 -------");
+				Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+				Console.WriteLine(string.Empty);
+
+				using (var stringContent = new StringContent(@"""content""", Encoding.UTF8, "application/json"))
+				{
+					response = httpClient.PostAsync("http://localhost:55055/API/values", stringContent).Result;
+				}
+
+				Console.WriteLine("POST -------");
+				Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+				Console.WriteLine(string.Empty);
+
+				using (var stringContent = new StringContent(@"""content""", Encoding.UTF8, "application/json"))
+				{
+					response = httpClient.PutAsync("http://localhost:55055/API/values/12", stringContent).Result;
+				}
+
+				Console.WriteLine("PUT -------");
+				Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+				Console.WriteLine(string.Empty);
+
+				using (var stringContent = new StringContent(@"""content""", Encoding.UTF8, "application/json"))
+				{
+					response = httpClient.DeleteAsync("http://localhost:55055/API/values/12").Result;
+				}
+
+				Console.WriteLine("DELETE -------");
+				Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+				Console.WriteLine(string.Empty);
+
+				response = httpClient.GetAsync("http://localhost:55055/API/values/filter?value=true").Result;
+
+				Console.WriteLine("GET ROUTE -------");
+				Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
